96eafed444b0eb9ad5a45bc02655f61dcc0adbb6
raiajit022 <raiajit022@gmail.com>
2024-12-02 14:00:52 +0530

    Fix import paths and Node.js version

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
   },
-  reactStrictMode: true
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
