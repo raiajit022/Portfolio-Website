cad89a430d0b538a29a82ab0f2ac2029e6594f0a
raiajit022 <raiajit022@gmail.com>
2024-12-11 14:01:45 +0530

    Fix: Update imports and build configuration

--- next.config.js
+++ next.config.js
@@ -21,11 +21,7 @@ const nextConfig = {
     turbotrace: {
       enabled: true,
     },
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
